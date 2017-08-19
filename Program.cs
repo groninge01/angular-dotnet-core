8eb06cd598180442ee4bd460e98b4eaeedfc41eb
Stefan van Groningen <groninge@hotmail.com>
2017-08-19 19:58:21 +0200

    Added ConfigurationBuilder for --urls switch

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplicationBasic
 {
@@ -11,11 +12,16 @@ namespace WebApplicationBasic
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseConfiguration(configuration)
                 .Build();
 
             host.Run();
